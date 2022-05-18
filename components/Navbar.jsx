ec3ebcdfa69323df1bea94b3db18b0b19ae15548
Fathurrachman D. Saputra <namasayafathur@gmail.com>
2022-05-18 14:52:18 +0700

    Adding detail

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,12 +11,12 @@ function NavbarComponent() {
           <div className="grow flex mx-3">
             <input type="text" className="border rounded-full pl-2 w-full grow" placeholder="Search" />
             <input type="text" className="border rounded-full pl-2 mx-3 w-96 hidden lg:inline" placeholder="Location" />
-            <button className="btn-primary">Search</button>
+            <button className="btn-primary mx-2">Search</button>
           </div>
           <Image src={user} height={50} width={50} />
         </div>
       </div>
-      <div className="bg-teal-600 h-7 mb-5"></div>
+      <div className="bg-teal-600 h-7 mb-5 "></div>
     </>
   )
 }
